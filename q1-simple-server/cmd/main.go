45b3ba3fabda9099598312bd8f4fb069b35524c4
Syed Jabeed <jabeedsyedinfo@gmail.com>
2026-01-10 17:23:28 +0530

    echo server

---
+++ q1-simple-server/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+// Hello World API: Create a simple Echo server with GET / that returns "Hello, World!".
+import "github.com/labstack/echo/v4"
+
+func main() {
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+
+		return c.String(200, "Hello world")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
